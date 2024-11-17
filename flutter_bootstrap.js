9b3aa1a549d47c1ad5384464f73485e8ff7d90fd
Aryan Sagavekar <sagavekararyan@gmail.com>
2024-11-17 19:51:51 +0530

    Fixed major issues and added profile and library pages
    Signed-off-by: Aryan Sagavekar <sagavekararyan@gmail.com>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1546257361"
+    serviceWorkerVersion: "2608503067"
   }
 });
